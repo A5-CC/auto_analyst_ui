996992e5f66b5ada628861625f9de11ab28c0284
a5-008 <alberto.blanco@atom5engineering.com>
2025-05-27 17:07:06 +0200

    feat: add Next.js configuration for export and asset handling

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import("next").NextConfig} */
+module.exports = {
+  output: "export",
+  trailingSlash: true,
+  basePath: "/auto_report_analyst_ui",
+  assetPrefix: "/auto_report_analyst_ui",
+  images: { unoptimized: true }
+};
